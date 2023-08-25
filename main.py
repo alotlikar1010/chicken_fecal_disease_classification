5634e179bba21fc5e6aa91f7457e2a8281a2e020
alotlikar1010 <alotlikar98@gmail.com>
2023-08-25 22:30:46 +0530

    data ingestion complete

---
+++ main.py
@@ -0,0 +1,19 @@
+from chicken import logger
+from chicken.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+
+# testing logging code 
+#logger.info("Welcome to custom logs")
+
+
+
+
+
+STAGE_NAME = "Data Ingestion stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = DataIngestionTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
