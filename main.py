d5dd84ecbcedf1e636c36c1c44a2c755e9218abe
alotlikar1010 <alotlikar98@gmail.com>
2023-08-26 10:52:58 +0530

    prepare base model complete

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from chicken import logger
 from chicken.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
-
+from chicken.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
 # testing logging code 
 #logger.info("Welcome to custom logs")
 
@@ -13,6 +13,19 @@ try:
    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
    data_ingestion = DataIngestionTrainingPipeline()
    data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+
+
+STAGE_NAME = "Prepare base model"
+try: 
+   logger.info(f"*******************")
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   prepare_base_model = PrepareBaseModelTrainingPipeline()
+   prepare_base_model.main()
    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
         logger.exception(e)
